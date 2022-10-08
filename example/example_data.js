30a71858a2dd6c3389eba04daf53ea68b9c70a24
lokeshpahal <lokeshpahal2000@gmail.com>
2022-10-08 22:21:09 +0530

    init code

---
+++ example/example_data.js
@@ -0,0 +1,121 @@
+let example_data = {
+	"blocks" : [
+        {
+            "id" : "ikgkm_-QMP",
+            "type" : "header",
+            "data" : {
+                "text" : "Example : @calumk/editorjs-columns ",
+                "level" : 3
+            }
+        },
+        {
+            "id" : "TiNrUV6dqT",
+            "type" : "paragraph",
+            "data" : {
+                "text" : "This is an example of using EditorJs, with the @calumk/editorjs-columns package"
+            }
+        },
+        {
+            "id" : "-COA6TMtAA",
+            "type" : "delimiter",
+            "data" : {}
+        },
+        {
+            "id" : "YWmRGUmuiI",
+            "type" : "columns",
+            "data" : {
+                "cols" : [
+                    {
+                        "time" : 1651768239769,
+                        "blocks" : [
+                            {
+                                "id" : "H7fLJcfXeJ",
+                                "type" : "header",
+                                "data" : {
+                                    "text" : "Column 1",
+                                    "level" : 3
+                                }
+                            },
+                            {
+                                "id" : "hJiCUezlKj",
+                                "type" : "paragraph",
+                                "data" : {
+                                    "text" : "Column 1"
+                                }
+                            }
+                        ],
+                        "version" : "2.24.2"
+                    },
+                    {
+                        "time" : 1651768239769,
+                        "blocks" : [
+                            {
+                                "id" : "0gdIy2MAYf",
+                                "type" : "header",
+                                "data" : {
+                                    "text" : "Column 2",
+                                    "level" : 3
+                                }
+                            },
+                            {
+                                "id" : "BW_XfHCpEq",
+                                "type" : "paragraph",
+                                "data" : {
+                                    "text" : "Column 2"
+                                }
+                            }
+                        ],
+                        "version" : "2.24.2"
+                    }
+                ]
+            }
+        },
+        {
+            "id" : "GzFVCoh1KM",
+            "type" : "columns",
+            "data" : {
+                "cols" : [
+                    {
+                        "time" : 1652191341357,
+                        "blocks" : [
+                            {
+                                "id" : "oRxIvvV0BU",
+                                "type" : "paragraph",
+                                "data" : {
+                                    "text" : "A"
+                                }
+                            }
+                        ],
+                        "version" : "2.24.3"
+                    },
+                    {
+                        "time" : 1652191341357,
+                        "blocks" : [
+                            {
+                                "id" : "a9taH31sEo",
+                                "type" : "paragraph",
+                                "data" : {
+                                    "text" : "B"
+                                }
+                            }
+                        ],
+                        "version" : "2.24.3"
+                    },
+                    {
+                        "time" : 1652191341357,
+                        "blocks" : [
+                            {
+                                "id" : "zJFadDCEbd",
+                                "type" : "paragraph",
+                                "data" : {
+                                    "text" : "C"
+                                }
+                            }
+                        ],
+                        "version" : "2.24.3"
+                    }
+                ]
+            }
+        }
+    ],
+};
